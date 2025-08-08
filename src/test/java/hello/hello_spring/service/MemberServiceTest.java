c1385ce108561a7abe96975758ae65b19fd33d81
ddooing <118183105+ddooing@users.noreply.github.com>
2025-08-08 16:24:51 +0900

    section 4 마무리

---
+++ src/test/java/hello/hello_spring/service/MemberServiceTest.java
@@ -0,0 +1,61 @@
+package hello.hello_spring.service;
+
+import com.jayway.jsonpath.internal.Utils;
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        //같은 메모리 멤버 리포지터리 사용됨 .. DI 의존성 주입
+        memberService = new MemberService(memberRepository);
+    }
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void 회원가입() throws Exception {
+        //Given
+        Member member = new Member();
+        member.setName("hello");
+        //When
+        Long saveId = memberService.join(member);
+        //Then
+        Member findMember = memberRepository.findById(saveId).get();
+        assertEquals(member.getName(), findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+    //Given
+    Member member1 = new Member();
+    member1.setName("spring");
+    Member member2 = new Member();
+    member2.setName("spring");
+
+    //When.. try-catch 문은 애매한 방법
+    memberService.join(member1);
+    IllegalStateException e = assertThrows(IllegalStateException.class,
+            () -> memberService.join(member2));//예외가 발생해야 한다.
+    assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
