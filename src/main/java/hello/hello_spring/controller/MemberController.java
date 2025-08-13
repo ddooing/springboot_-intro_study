00eb3b7acb0cb48b7d4a84de66ec97a68e457fe1
ddooing <118183105+ddooing@users.noreply.github.com>
2025-08-14 00:02:44 +0900

    2. 회원 등록 폼

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hello_spring.controller;
 
+import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -13,17 +16,20 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
-    
-    /*//필드 주입 방법.. 비추
-    @Autowired private MemberService memberService;
-    */
 
-    /*// setter 주입 .. public 으로 노출됨
-    @Autowired
-    public void setMemberService(MemberService memberService)
-    {
-        this.memberService=memberService;
-    }*/
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
+
 
 
 }
